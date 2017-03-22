6fd31fdb6322718c96444d9486e93e2b37870328
Billy Wilson Arante <arantebillywilson@gmail.com>
2017-03-22 17:17:27 +0800

    Created CC license

--- templates/landing.php
+++ templates/landing.php
@@ -16,7 +16,7 @@
 					<div class='col-lg-6'>
 						<p>Footer left column</p>
 					</div>
-					<div class='col-lg-6'>
+					<div class='col-lg-6 text-right'>
 						<p>Created by <a href='#'>Billy Wilson Arante</a> of Mandaluyong City, Philippines</p>
 						<p>Powered by <a href='#'>Tuitt Coding Bootcamp</a></p>
 
@@ -25,6 +25,9 @@
 						<a href='#'><i class='fa fa-facebook-official fa-2x footer-fa' aria-hidden='true'></i></i></a>
 						<a href='#'><i class='fa fa-medium fa-2x footer-fa' aria-hidden='true'></i></i></a>
 						<a href='#'><i class='fa fa-youtube-play fa-2x footer-fa' aria-hidden='true'></i></i></a>
+
+						<br><br><br>
+						<a rel='license' href='http://creativecommons.org/licenses/by-sa/4.0/'><img alt='Creative Commons License' style='border-width:0' src='https://i.creativecommons.org/l/by-sa/4.0/88x31.png' /></a><br />This work is licensed under a <a rel='license' href='http://creativecommons.org/licenses/by-sa/4.0/'>Creative Commons Attribution-ShareAlike 4.0 International License</a>.
 					</div>
 				</div>
 			</div>
