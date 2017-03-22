5ee7bf88b37be67e4a350bd6364af25933830c38
Billy Wilson Arante <arantebillywilson@gmail.com>
2017-03-22 17:08:20 +0800

    templates: Initial commit

---
+++ templates/landing.php
@@ -0,0 +1,36 @@
+<!-- templates/landing.php -->
+<?php $title = "Beeer | Home" ?>
+
+<?php
+
+	$header = "
+		<div class='container-fluid'>
+			<a href='#'>beeer</a>
+		</div>
+	";
+
+	$footer = "
+		<div class='containter-fluid'>
+			<div class='container'>
+				<div class='row'>
+					<div class='col-lg-6'>
+						<p>Footer left column</p>
+					</div>
+					<div class='col-lg-6'>
+						<p>Created by <a href='#'>Billy Wilson Arante</a> of Mandaluyong City, Philippines</p>
+						<p>Powered by <a href='#'>Tuitt Coding Bootcamp</a></p>
+
+						<a href='#'><i class='fa fa-github-alt fa-2x footer-fa' aria-hidden='true'></i></a>
+						<a href='#'><i class='fa fa-stack-overflow fa-2x footer-fa' aria-hidden='true'></i></a>
+						<a href='#'><i class='fa fa-facebook-official fa-2x footer-fa' aria-hidden='true'></i></i></a>
+						<a href='#'><i class='fa fa-medium fa-2x footer-fa' aria-hidden='true'></i></i></a>
+						<a href='#'><i class='fa fa-youtube-play fa-2x footer-fa' aria-hidden='true'></i></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	";
+
+?>
+
+<?php include "layout.php" ?>
---
+++ templates/layout.php
@@ -0,0 +1,24 @@
+<!-- templates/layout.php -->
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title><?= $title ?></title>
+
+	<!-- Bootstrap -->
+	<link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+
+	<!-- Personal -->
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+	
+	<?= $header ?>
+
+	<?= $footer ?>
+
+	<!-- Font awesome -->
+	<script src="https://use.fontawesome.com/63421b52b2.js"></script>
+
+</body>
+</html>
