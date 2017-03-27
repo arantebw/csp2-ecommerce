a9cbb0ddd586a6f069b929043c6a5c9c018649f2
Billy Wilson Arante <arantebillywilson@gmail.com>
2017-03-27 17:34:40 +0800

    Created dead link to brand name

--- tmp/main_header.php
+++ tmp/main_header.php
@@ -1,11 +1,11 @@
-<div class='container-fluid main-header'>
-	<div class='container'>
-		<div class='row'>
-			<div class='col-lg-6'>
-				<a class='brand-name'>beeer</a>
+<div class="container-fluid main-header">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6">
+				<a class="brand-name" href="#">beeer</a>
 			</div>
-			<div class='col-lg-6 text-right main-header-right-col'>
-				<button class='btn btn-default'><i class='fa fa-facebook' aria-hidden='true' style='margin-right:10px;'></i> Sign in with Facebook</button>
+			<div class="col-lg-6 text-right main-header-right-col">
+				<button class="btn btn-default"><i class="fa fa-facebook" aria-hidden="true" style="margin-right:10px;"></i> Sign in with Facebook</button>
 				<button class="btn btn-default"><i class="fa fa-google" aria-hidden="true" style="margin-right:10px"></i> Sign in with Google</button>
 			</div>
 		</div>
