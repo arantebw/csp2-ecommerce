a1df232706c8a7c98e9b8154e3ad84f14bc1a6da
Billy Wilson Arante <arantebillywilson@gmail.com>
2017-03-27 18:38:26 +0800

    Created search tool

--- tmp/hero_banner.php
+++ tmp/hero_banner.php
@@ -2,27 +2,38 @@
     <div class="home-explorer">
       <h1>Discover local craft beers</h1>
       <span>of the Philippines</span>
+
+      <div class="row search-tool">
+        <div class="col-lg-12">
+          <div class="input-group">
+            <input type="text" class="form-control form-control-personal" placeholder="Find craft beers">
+            <span class="input-group-btn">
+              <button class="btn btn-default btn-default-personal"><i class="fa fa-search" aria-hidden="true"></i></button>
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
 
     <div class="carousel-inner" role="listbox" style="height:600;">
       <div class="item active">
-        <img src="./img/beer1.jpg" width="100%">      
+        <img src="./img/beer1.jpg" width="100%">
       </div>
 
       <div class="item">
-        <img src="./img/beer2.jpg" width="100%">      
-      </div>
-    
-      <div class="item">
-        <img src="./img/beer3.jpg" width="100%">      
+        <img src="./img/beer2.jpg" width="100%">
       </div>
 
       <div class="item">
-        <img src="./img/beer4.jpg" width="100%">      
+        <img src="./img/beer3.jpg" width="100%">
       </div>
 
       <div class="item">
-        <img src="./img/beer5.jpg" width="100%">      
+        <img src="./img/beer4.jpg" width="100%">
+      </div>
+
+      <div class="item">
+        <img src="./img/beer5.jpg" width="100%">
       </div>
     </div>
 </div>
