4bef568fb9638a1779a2f1811080dbd3357a395e
Billy Wilson Arante <arantebillywilson@gmail.com>
2017-03-28 23:37:42 +0800

    Applied img for brand name

--- tmp/main_header.php
+++ tmp/main_header.php
@@ -2,10 +2,10 @@
 <div class="container-fluid main-header">
 	<div class="container">
 		<div class="row">
-			<div class="col-lg-6">
-				<a class="brand-name" href="#">beeer</a>
+			<div class="col-xs-6">
+				<a class="brand-name" href="#"><img src="./img/beeer_logo.png"></a>
 			</div>
-			<div class="col-lg-6 text-right main-header-right-col">
+			<div class="col-xs-6 text-right main-header-right-col">
 				<button class="btn btn-default"><i class="fa fa-facebook" aria-hidden="true" style="margin-right:10px;"></i> Sign in with Facebook</button>
 				<button class="btn btn-default"><i class="fa fa-google" aria-hidden="true" style="margin-right:10px"></i> Sign in with Google</button>
 			</div>
